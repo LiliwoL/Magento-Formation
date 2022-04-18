2a8e6e2f6ae3f057bffacd49964e1f921cf40efb
Nicolas <github@liliwol.fr>
2022-04-18 21:31:37 +0200

    Fiurst

---
+++ modules/FormationMagento/ModuleBlock/Controller/Test/Index.php
@@ -0,0 +1,11 @@
+<?php
+namespace FormationMagento\ModuleBlock\Controller\Test;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        $this->_view->loadLayout();
+        $this->_view->renderLayout();
+    }
+}
