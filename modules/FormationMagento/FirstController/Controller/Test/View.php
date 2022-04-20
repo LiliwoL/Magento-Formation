4b8b6514893691ba75d1c432e1a55426235e300d
Nicolas <github@liliwol.fr>
2022-04-20 15:15:39 +0200

    hop

---
+++ modules/FormationMagento/FirstController/Controller/Test/View.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FormationMagento\FirstController\Controller\Test;
+
+class View extends \Magento\Framework\App\Action\Action
+{
+	public function execute()
+	{
+		die('Une autre action');
+	}
+}
