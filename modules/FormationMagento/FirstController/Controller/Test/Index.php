0ea47eb8e0139ec080dc0363a6062aa591c3abe3
Nicolas <github@liliwol.fr>
2022-04-20 14:27:59 +0200

    FirstController

---
+++ modules/FormationMagento/FirstController/Controller/Test/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FormationMagento\FirstController\Controller\Test;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+	public function execute()
+	{
+		die('Test controller');
+	}
+}
