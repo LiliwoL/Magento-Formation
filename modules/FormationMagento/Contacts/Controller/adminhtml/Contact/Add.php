7f16bdd6df7b2934bf64d444f85e39c449804e18
Nicolas <github@liliwol.fr>
2022-04-21 14:26:44 +0200

    hop

---
+++ modules/FormationMagento/Contacts/Controller/adminhtml/Contact/Add.php
@@ -0,0 +1,9 @@
+<?php
+namespace FormationMagento\Contacts\Controller\Adminhtml\Contact;
+use Magento\Backend\App\Action;
+
+class Add extends \Magento\Backend\App\Action {
+    public function execute() {
+       die('test add');
+    }
+}
